eda13b052a1d400f128916bd2c21f2df44b4a6fa
ben.huang52 <ben.huang@titansoft.com.sg>
2019-09-26 10:45:43 +0800

    Create project

---
+++ DeadAntsTest/DeadAntTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeadAntsTest
+{
+    [TestClass]
+    public class DeadAntTests
+    {
+        private readonly DeadAnts _deadAnts = new DeadAnts();
+
+        [TestMethod]
+        public void NoAnt()
+        {
+            var deadAntsCount = _deadAnts.DeadAntsCount("");
+
+            Assert.AreEqual(0, deadAntsCount);
+        }
+
+        [TestMethod]
+        public void OneAntDead()
+        {
+            var deadAntsCount = _deadAnts.DeadAntsCount("a nt");
+
+            Assert.AreEqual(0, deadAntsCount);
+        }
+    }
+
+    public class DeadAnts
+    {
+        public int DeadAntsCount(string antsString)
+        {
+            return 0;
+        }
+    }
+}
---
+++ DeadAntsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DeadAntsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DeadAntsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f339da53-ac69-44e5-b391-09c60b2c7e5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
